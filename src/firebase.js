4631d6b94b535c045f6e4dc6018526a04079b839
Waleed <waleedeveloper100@gmail.com>
2025-07-31 11:14:33 +0500

    firebase changes done

--- src/firebase.js
+++ src/firebase.js
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDKXWLKgMZnrNlMg3oerSU0SNrgV7mjHm4",
-    authDomain: "dashboardauth-49faa.firebaseapp.com",
-    projectId: "dashboardauth-49faa",
-    storageBucket: "dashboardauth-49faa.appspot.com",
-    messagingSenderId: "419176969951",
-    appId: "1:419176969951:web:4e2df5e7dce28d5878db6a",
-    measurementId: "G-7NBBJ8F052"
+    apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+    authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+    projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+    storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+    appId: import.meta.env.VITE_FIREBASE_APP_ID,
+    measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
