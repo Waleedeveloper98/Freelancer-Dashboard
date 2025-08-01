727f5299bf8b2fffa054b338e91d5d90eafe5eb5
Waleed <waleedeveloper100@gmail.com>
2025-08-01 21:40:40 +0500

    firebase error resolved

--- src/context/AuthContextProvider.jsx
+++ src/context/AuthContextProvider.jsx
@@ -5,7 +5,8 @@ import {
   signInWithEmailAndPassword,
   signOut,
 } from "firebase/auth";
-import { auth } from "../firebase";
+import { auth, db } from "../firebase";
+import { doc, getDoc, setDoc } from "firebase/firestore";
 
 export const authContext = createContext();
 
@@ -15,16 +16,39 @@ const AuthContextProvider = ({ children }) => {
   const [showPassword, setShowPassword] = useState(false);
 
   useEffect(() => {
-    const unsub = onAuthStateChanged(auth, (currentUser) => {
+    const unsub = onAuthStateChanged(auth, async (currentUser) => {
       setUser(currentUser);
-      setLoading(false)
+      setLoading(false);
+
+      if (currentUser) {
+        const docRef = doc(db, "users", currentUser.uid);
+        const docSnap = await getDoc(docRef);
+      }
+      if (docSnap.exists()) {
+        const userData = docSnap.data();
+        console.log("🔥 User data loaded:", userData);
+      } else {
+        console.log("❌ No user data found");
+      }
     });
     return () => unsub();
   }, []);
 
   // Sign up function
-  const signup = (email, password) => {
-    return createUserWithEmailAndPassword(auth, email, password);
+  const signup = async (email, password) => {
+    const userCredntial = createUserWithEmailAndPassword(auth, email, password);
+    const user = userCredntial.user;
+
+    await setDoc(doc(db, "users", user.uid), {
+      email: user.email,
+      createdAt: new Date().toISOString(),
+      dashboardData: {
+        projects: [],
+        totalSpent: 0,
+        status: "active",
+      },
+    });
+    return userCredntial;
   };
 
   // Login function
@@ -51,7 +75,7 @@ const AuthContextProvider = ({ children }) => {
         showPassword,
         setShowPassword,
         showHidePassword,
-        loading
+        loading,
       }}
     >
       {children}
